858f5cbac192554a74c75212136b4afc996a8eac
Peter Nduati <lnduatipeter@gmail.com>
2018-09-26 12:16:25 +0300

    Build Latest images and create tags from HEAD

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+docker build -t pnduati/multi-client:latest -t pnduati/multi-client:$GIT_SHA -f  ./client/Dockerfile ./client
+docker build -t pnduati/multi-server:latest -t pnduati/multi-server:$GIT_SHA -f  ./server/Dockerfile ./server
+docker build -t pnduati/multi-worker:latest -t pnduati/multi-worker:$GIT_SHA -f  ./worker/Dockerfile ./worker
+
+docker push pnduati/multi-client:latest
+docker push pnduati/multi-server:latest
+docker push pnduati/multi-worker:latest
+
+docker push pnduati/multi-client:$GIT_SHA
+docker push pnduati/multi-server:$GIT_SHA
+docker push pnduati/multi-worker:$GIT_SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=pnduati/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment server=pnduati/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment server=pnduati/multi-worker:$GIT_SHA
